8d3a72b0cd19a1c835d7c0d2a3ba27d66fbb757f
g a veda vyas <ayyappavedavyas.gudipati@gmail.com>
2026-01-28 11:42:38 +0530

    basic variable and condition scripting

---
+++ 01-shellscripting.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script demonstrates basic shell scripting concepts
+
+# 1. Variables
+name="vedavyas"
+echo "Hi this is $name"
+
+# 2. Conditional Statements
+age=$2
+
+if [ $age -lt 26 ]; then
+    echo "approved welcome to devops world"
+else
+    echo "You are not approved!"
+fi  
