63a13153b000faacb627a310014e38d2eb56e268
ayyappavedavyasgudipati <ayyappavedavyas.gudipati@gmail.com>
2026-02-01 15:07:29 +0530

    updating records

--- shell-roboshop/roboshop-services.sh
+++ shell-roboshop/roboshop-services.sh
@@ -3,6 +3,8 @@
 SG_ID="sg-08ead4da4a18d399e"
 AMI_ID="ami-0220d79f3f480ecf5"
 INSTANCE_TYPE="t3.micro"
+ZONEID="Z10201011EZM611L941TT"
+DNS="opswithvyas.online"
 
 
 for INSTANCE in $@
@@ -22,13 +24,42 @@ do
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text) 
+
+        RECORD_NAME="$DNS"    
     else 
         echo "$INSTANCE PRIVATE IP"
         IP=$(aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PrivateIpAddress' \
-            --output text)  
+            --output text) 
+        RECORD_NAME="$INSTANCE.$DNS"       
     fi 
 
-    echo "$IP"              
+    echo "$IP"      
+
+     aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+
+    echo "record updated for $INSTANCE"
+
 done
